30eafd2b1109695ec3da360f6e784847cc420968
Niranjan Manjunath <Niranjan.Somappa-Manjunath@rabobank.nl>
2021-01-27 15:37:52 +0100

    Add upload and download tests

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-from main import get_client, list_buckets, create_bucket, call_everything
+from main import get_client, list_buckets, create_bucket, upload_file, download_file, call_everything
 from moto import mock_s3
 
 @mock_s3
@@ -6,6 +6,11 @@ def test_get_client():
     s3 = get_client()
     create_bucket(s3, "bucket")
     
+@mock_s3
+def test_create_bucket():
+    s3 = get_client()
+    
+    create_bucket(s3, "testbucket1")
 
 @mock_s3
 def test_list_buckets():
@@ -13,6 +18,21 @@ def test_list_buckets():
     
     bucketlist = list_buckets(s3)
     print(f"Nirus: {bucketlist}")  
+
+@mock_s3
+def test_upload_file():
+
+    s3 = get_client()
+    create_bucket(s3, "testbucket1")
+    upload_file(s3, "testbucket1")
+
+@mock_s3
+def test_download_file():
+
+    s3 = get_client()
+    create_bucket(s3, "testbucket1")
+    upload_file(s3, "testbucket1")
+    download_file(s3, "testbucket1") 
 
 @mock_s3
 def test_call_everything():
