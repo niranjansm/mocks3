bb1e5431b4521112baf72cbd56cb34cc177d1d7e
Niranjan Manjunath <Niranjan.Somappa-Manjunath@rabobank.nl>
2021-01-27 21:18:52 +0100

    Add pytest.ini Modify tests to include assert statements

--- test_main.py
+++ test_main.py
@@ -1,38 +1,39 @@
-from main import get_client, list_buckets, create_bucket, upload_file, download_file, call_everything
+from main import get_client, list_buckets, create_bucket
+from main import upload_file, download_file, list_bucket_objects, call_everything
 from moto import mock_s3
+import os
 
-@mock_s3
-def test_get_client():
-    s3 = get_client()
-    create_bucket(s3, "bucket")
-    
-@mock_s3
-def test_create_bucket():
-    s3 = get_client()
-    
-    create_bucket(s3, "testbucket1")
 
 @mock_s3
 def test_list_buckets():
     s3 = get_client()
-    
-    bucketlist = list_buckets(s3)
-    print(f"Nirus: {bucketlist}")  
+    bucketlist = ["testbucket1", "testbucket2"]
+    create_bucket(s3, bucketlist[0])
+    create_bucket(s3, bucketlist[1])
+    response = list_buckets(s3)
+    assert response ==  bucketlist
 
 @mock_s3
 def test_upload_file():
 
     s3 = get_client()
-    create_bucket(s3, "testbucket1")
-    upload_file(s3, "testbucket1")
+    bucket_name = "testbucket"
+    create_bucket(s3, bucket_name)
+    upload_file(s3, bucket_name)
+
+    objects = list_bucket_objects(s3, bucket_name)
+
+    assert ["hello.txt"] == objects
 
 @mock_s3
 def test_download_file():
 
     s3 = get_client()
-    create_bucket(s3, "testbucket1")
-    upload_file(s3, "testbucket1")
-    download_file(s3, "testbucket1") 
+    bucket_name = "testbucket"
+    create_bucket(s3, bucket_name)
+    upload_file(s3, bucket_name)
+    download_file(s3, bucket_name)
+    assert os.path.exists("downloadedhello.txt")
 
 @mock_s3
 def test_call_everything():
